ef0873e1d84f4b5a96589615dbea04a566c4598e
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2023-11-28 10:56:50 +0100

    Add search functionality to equipment list

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialListComponent } from './components/material-list/material-list.component';
 import { AdminRoutingModule } from './admin-routing.module'; // Assurez-vous que le chemin est correct
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [MaterialListComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [CommonModule, AdminRoutingModule, FormsModule],
 })
 export class AdminModule {}
--- src/app/admin/components/material-list/material-list.component.ts
+++ src/app/admin/components/material-list/material-list.component.ts
@@ -9,12 +9,26 @@ import { AdminService } from '../../../core/services/admin.service';
 })
 export class MaterialListComponent implements OnInit {
   materials: Material[] = [];
+  filteredMaterials: Material[] = [];
+  searchQuery: string = '';
 
   constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
     this.adminService.getMaterials().subscribe((materials) => {
       this.materials = materials;
+      this.filteredMaterials = materials;
     });
+  }
+  onSearch(): void {
+    if (!this.searchQuery) {
+      this.filteredMaterials = this.materials;
+    } else {
+      this.filteredMaterials = this.materials.filter((material) =>
+        material.equipment
+          .toLowerCase()
+          .includes(this.searchQuery.toLowerCase())
+      );
+    }
   }
 }
