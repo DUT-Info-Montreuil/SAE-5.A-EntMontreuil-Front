9c472518a36fadcca27d9cbe5a0b618b8548bec9
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2023-12-03 15:08:27 +0100

    te

--- src/app/timetable/timetable.module.ts
+++ src/app/timetable/timetable.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimetableLayoutComponent } from './components/timetable-layout/timetable-layout.component';
 import { TimetableRoutingModule } from './timetable-routing.module';
-
-
+import { DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarModule } from 'angular-calendar';
+import { SchedulerModule } from 'angular-calendar-scheduler';
+import { TimetableService } from '../core/services/timetable.service';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,14 @@ import { TimetableRoutingModule } from './timetable-routing.module';
   ],
   imports: [
     CommonModule,
-    TimetableRoutingModule
-  ]
+    TimetableRoutingModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    SchedulerModule.forRoot({ locale: 'fr', headerDateFormat: 'daysRange', logEnabled: true })
+  ],
+  providers: [
+    TimetableService,
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ],
+  bootstrap: [TimetableLayoutComponent]
 })
 export class TimetableModule { }
