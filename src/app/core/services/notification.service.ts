77a1b8fbeb24b51237dda96c1b5294ee2b20cf7f
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2023-12-05 14:37:48 +0100

    limiter l'affichage du nombre de ntoifications

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -24,7 +24,7 @@ export class NotificationService {
     constructor(private http: HttpClient) { }
 
     readNotifications(): Observable<Notification[]> {
-        return this.http.get<{ notifications: any[]; totalUnread: number }>(this.apiURL + '/user/notifications?reading=true', this.httpOptions)
+        return this.http.get<{ notifications: any[]; totalUnread: number }>(this.apiURL + '/user/notifications?reading=true&display=5', this.httpOptions)
             .pipe(
                 tap(response => {
                     this.notificationsSource.next(response.notifications.map(notif => notif.notification));
@@ -35,7 +35,7 @@ export class NotificationService {
     }
 
     getNotifications(): Observable<Notification[]> {
-        return this.http.get<{ notifications: any[]; totalUnread: number }>(this.apiURL + '/user/notifications', this.httpOptions)
+        return this.http.get<{ notifications: any[]; totalUnread: number }>(this.apiURL + '/user/notifications&display=5', this.httpOptions)
             .pipe(
                 tap(response => {
                     this.notificationsSource.next(response.notifications.map(notif => notif.notification));
