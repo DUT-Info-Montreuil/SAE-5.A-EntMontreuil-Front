6109c1c629b50e638db4673c24aa806613ce8362
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2023-12-13 01:42:32 +0100

    Update cohort.component.ts

--- src/app/resp/components/cohort/cohort.component.ts
+++ src/app/resp/components/cohort/cohort.component.ts
@@ -13,6 +13,8 @@ import { CohortService } from 'src/app/core/services/cohort.service';
       height: 88dvh;
       overflow: auto;
       overflow-y: hidden;
+      border:none;
+      box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.05), 0px 1px 4px rgba(0, 0, 0, 0.08) !important;
   }
   
   :host ::ng-deep .p-tree:hover {
